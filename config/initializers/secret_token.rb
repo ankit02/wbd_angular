be58503b821ffb6b4ff5b16ae872a35d7db8c8ee
Ankit Seth <ankit@ubuntu.(none)>
2014-03-10 04:49:49 -0700

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wbd::Application.config.secret_key_base = '7c5d76739dfacbf978641d11cb083ed6ee1d9b6a1b5714e259a00b982de610486bc711e8ba163775bb5c3ca9bf7fc4d9015986ff42b5de59ffe9bf20b1c53abb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wbd::Application.config.session_store :cookie_store, key: '_wbd_session'
